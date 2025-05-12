486dea27b61b2d4f4cf0a6f9c25f14323c453fcd
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-05-12 18:34:00 +0500

    multer added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MulterModule } from '@nestjs/platform-express';
+import { join } from 'path';
+import { MulterController } from './multer/multer.controller';
 
 @Module({
-  imports: [AuthorModule, BookModule, PrismaModule],
-  controllers: [AppController],
+  imports: [
+    AuthorModule,
+    BookModule,
+    PrismaModule,
+    MulterModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/file',
+    }),
+  ],
+  controllers: [AppController,MulterController],
   providers: [AppService],
 })
 export class AppModule {}
